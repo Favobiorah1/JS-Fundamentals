dc99c466ee1a74b375b3ade14e7c717a31c80507
Favour Obiorah <favobiorah3@gmail.com>
2025-07-14 15:27:02 -0700

    add 1-multi_language.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,1 +1,8 @@
-
+public class main{
+	public static void
+		main(string[] args) {
+			const myvar ="javascript is amazing";
+	console.log(myvar);
+	system.out.println(myvar);
+	}
+}
--- 1-multi_language.js
+++ 1-multi_language.js
@@ -1,1 +1,13 @@
 
+public class main{
+	public static void
+main(string[] args) 
+	const myvar = " C is fun";
+	const myvar = "python is cool";
+	const myvar = "javascript is amazing";
+console.log("C is fun");
+console.log("python is cool");
+console.log("javascript is amazing");
+   }
+}
+
