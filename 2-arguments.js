d11bba39dc4cbdd75b7d8f9de3c306fae6b9b7c3
Favour Obiorah <favobiorah3@gmail.com>
2025-07-14 16:04:22 -0700

    add 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 
+=======
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+>>>>>>> add 2-arguments.js
