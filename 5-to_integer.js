507b14ef69a18b38638c0292b559d541c2313c2e
Favobiorah1 <favobiorah3@gmail.com>
2025-07-15 14:15:44 -0700

    add 6-multi_language_loop.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-public class main{
-	public static void
+#!/usr/bin/node
 
 const arg = process.argv[2];
 const num = parseInt(arg);
@@ -8,8 +6,7 @@ const num = parseInt(arg);
 if (!isNaN(num)) {
   console.log(`My number: ${num}`);
 } else {
-  console.log('Not a number');
+  console.log('Not a number')
 }
-=======
+  }
 
->>>>>>> 25ce1df71deca59c61cff534b646434579974a98
